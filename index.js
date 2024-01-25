64a0a61a288d5561b774a8f5645f247f5d16c78b
Mrinal Deb <mrinal437a@gmail.com>
2024-01-25 23:31:15 +0530

    added encoding with multer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const app= express();
 const port = 8000;
@@ -7,10 +9,17 @@ const port = 8000;
 app.set('view engine','ejs');
 app.set('views',path.resolve('./views'));
 
-app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
     return res.render('homepage.ejs');
+})
+
+
+app.post('/upload',upload.single('profileImage'),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    return res.redirect('/');
 })
 
 app.listen(port,()=>console.log(`server started on port = ${port}`));
